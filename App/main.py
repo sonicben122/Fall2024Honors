f3583ee416e81fdce52f836c1f80edaff582b46e
Ben <benparoulek@gmail.com>
2024-12-05 21:49:23 -0500

    Added folders for app

---
+++ App/main.py
@@ -0,0 +1,18 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+
+
+class BoxLayoutExample(BoxLayout):
+    pass
+
+class MainWidget(Widget):
+    pass 
+
+
+class ClassView(App):
+    pass
+
+
+ClassView().run()
